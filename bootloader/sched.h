4d1f109688e52c5c3d7ce47a6e6a803a66a8be79
tugmirk <tmirac@hotmail.com>
2026-05-26 16:12:03 +0300

    sched: add scheduler public API header

---
+++ bootloader/sched.h
@@ -0,0 +1,32 @@
+#ifndef SCHED_H
+#define SCHED_H
+#include <stdint.h>
+
+#define MAX_TASKS 4
+#define STACK_SZ  512
+
+#define TASK_FREE    0
+#define TASK_READY   1
+#define TASK_RUNNING 2
+
+typedef struct {
+    uint32_t sp;
+    uint8_t  state;
+    uint8_t  id;
+    uint8_t  pad[2];
+} tcb_t;
+
+extern tcb_t   tasks[MAX_TASKS];
+extern uint8_t cur_task;
+extern uint8_t task_stacks[MAX_TASKS][STACK_SZ];
+
+/* Returns task id (>=0) or -1 if no free slot. */
+int  task_create(void (*entry)(void));
+
+/* Sets cur_task=0, loads timer countdown, jumps to task 0. Never returns. */
+void sched_start(void);
+
+/* Called from irq_trampoline in start.S after qreg save. */
+void irq_handler(void);
+
+#endif
